b42147921f5e876cee81b8d2c6a11c4541808bef
Igoris Ivanovas <igoris.x@inbox.lt>
2021-10-09 18:43:27 +0300

    all setup for work with vue

--- public/js/app.js
+++ public/js/app.js
@@ -2081,11 +2081,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
-  mounted: function mounted() {
-    console.log("Component mounted.");
-  }
-});
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({});
 
 /***/ }),
 
